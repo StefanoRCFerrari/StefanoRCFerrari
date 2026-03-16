c7387150b14b4fae6269e2e593ebf74b8b7c91d5
Stéfano Ferrari <stefanorcferrari@gmail.com>
2026-03-16 10:27:18 -0300

    Add files via upload

--- AtvMod01Stefano/src/atvmod01stefano/AtvMod01Stefano.java
+++ AtvMod01Stefano/src/atvmod01stefano/AtvMod01Stefano.java
@@ -4,8 +4,33 @@ import java.util.Random;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
 
 public class AtvMod01Stefano {
+    
+    public static void imprimirLista(List<String> lista1) {
+            for (String item : lista1) {
+                System.out.println(item);
+        }
+    }
+     
+    public static void converterMaiusculas(List<String> lista1) {
+        lista1.replaceAll(String::toUpperCase);
+    }
+    
+    public static void excluirItensDuplicados(List<String> lista1) {
+        Set<String> set = new LinkedHashSet<>(lista1);
+        lista1.clear();
+        lista1.addAll(set);
+    }
+    
+    public static void imprimirListaReversa(List<String> lista1) {
+        for (int i = lista1.size() - 1; i >= 0; i--) {
+            System.out.println(lista1.get(i));
+    }
+}
     
     public static void main(String[] args) {
         
@@ -61,6 +86,16 @@ public class AtvMod01Stefano {
         
         coresL1.addAll(coresL2);
         coresL2 = null;
+        
+        imprimirLista(coresL1);
+        converterMaiusculas(coresL1);
+        imprimirLista(coresL1);
+        Collections.sort(coresL1);
+        imprimirLista(coresL1);
+        excluirItensDuplicados(coresL1);
+        imprimirLista(coresL1);
+        imprimirListaReversa(coresL1);
+        
     }
     
 }
