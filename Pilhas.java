4287c521ef6c562318e1d0b0280b3741c727afd7
Stéfano Ferrari <stefanorcferrari@gmail.com>
2026-04-14 08:52:58 -0300

    Add files via upload

--- Pilhas.java
+++ Pilhas.java
@@ -1,13 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package javaapplication3;
 
-/**
- *
- * @author stefano.ferrari
- */
 public class Pilhas {
     
+    private static final int max = 50;
+    private int topo = -1;
+    private char[] pilha = new char[max];
+    private char temp;
+     
+    public Pilhas(){
+        this.topo = -1;
+    }
+     
+    public boolean isFull(){
+        return this.topo == max-1;
+    }
+     
+    public boolean isEmpty(){
+        return this.topo == -1;
+    }
+
+    public void push(char elemento){
+        if (!this.isFull()){
+            topo++;
+            pilha[topo] = elemento;
+        }
+        else
+            System.out.println("Pilha cheia!");
+     }
+    
+    public char pop(){
+        if (!this.isEmpty()){
+            temp = pilha[topo];
+            pilha[topo] = '\0';
+            topo--;
+            return temp;
+        }
+        else{
+            System.out.println("Pilha vazia!");
+            return '\0';
+        }
+     }
+    
+    public char top(){
+        if (!this.isEmpty()){
+            return this.pilha[this.topo];
+        }
+        else{
+            System.out.println("Pilha vazia!");
+            return '\0';
+        }
+     }
 }
+    
